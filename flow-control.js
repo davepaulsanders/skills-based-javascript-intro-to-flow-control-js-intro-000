f882c131e270b3a9712b59237c25fff2aa5d079c
David P Sanders <davepaulsanders@gmail.com>
2021-03-25 15:03:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,15 @@
 function basicTeenager(age) {
 if (age >= 13 && <= 19){
 
-return `'You are a teenager!'`
-}
+return `You are a teenager!`
+} else
 }
 function teenager(age) {
-  if (age >= 13 && age <= 19){
+  if (age >= 13 && <= 19){
 
-  return `"You are a teenager!"`
+  return `You are a teenager!`
+} else {
+  return `You are not a teenager`
 }
 }
 
