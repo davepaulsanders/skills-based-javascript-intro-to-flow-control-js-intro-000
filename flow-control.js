8764cac566b3613922eec55985bb26d9d31faa92
David P Sanders <davepaulsanders@gmail.com>
2021-03-25 15:10:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,19 +5,11 @@ return `You are a teenager!`
 }
 }
 function teenager(age) {
-  if (age >= 13 && <= 19){
 
-  return `You are a teenager!`
-} else {
-  return `You are not a teenager`
-}
 }
 
 function ageChecker(age) {
-  if(age >= 13 && <= 19) {
-
-    return `'You are a teenager!'`
-  } else if (age)
+  
 }
 
 
